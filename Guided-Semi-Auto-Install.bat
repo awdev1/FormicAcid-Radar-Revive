fd63f95cfb8011632a52cedb0bfc2955c33e2041
awdev1 <165525907+awdev1@users.noreply.github.com>
2024-05-02 02:09:10 -0700

    Update Guided-Semi-Auto-Install.bat

--- Guided-Semi-Auto-Install.bat
+++ Guided-Semi-Auto-Install.bat
@@ -14,7 +14,7 @@ echo "     \/                    \/        \/        \/     \/        \/        
 rem Intro Screen
 echo ======================================================
 echo      Welcome to the FormicRadar Installer Script
-echo                Made by: awdev 
+echo                Made by: awdev (reviewed by Beani and auto install by DangerChamp)
 echo ======================================================
 echo.
 echo IF YOU WOULD RATHER FOLLOW A VIDEO GO TO BEANI'S CHANNEL VIA THE LINK POSTED IN THE PTFS TOOLS Server
@@ -39,8 +39,9 @@ if %errorlevel% equ 0 (
     echo Node.js is installed
 ) else (
     echo Node.js is not installed
-    echo Install Node.js by running the installer, then click Yes on the UAC Prompt
-    start node-v21.7.1-x64.msi
+    echo Installing Node.js. Click yes on the UAC Prompt
+    powershell Invoke-WebRequest https://nodejs.org/dist/v22.0.0/node-v22.0.0-x64.msi -O nodejs.msi
+    start nodejs.msi
     PAUSE
 )
 
@@ -52,19 +53,23 @@ if %errorlevel% equ 0 (
         echo Python 3.10 is installed
     ) else (
         echo Python 3.10 is not installed
-        echo Install Python 3.10 by running the installer, then click Yes on the UAC Prompt
+        echo Click Yes on the UAC Prompt
         cd _redist
-        start python-3.10.0-amd64.exe
+        powershell Invoke-WebRequest https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe -O python310.exe
+        start python310.exe
+        echo You seem to need to install python twice. This time thru the microsoft store. Press any button to start the MS store and click install
+        start ms-windows-store://pdp/?ProductId=9ncvdn91xzqp
         PAUSE
-        echo You might need to install from the microsoft store. So install both and go the the microsoft store and search for python 3.1 and you should be fine
     )
 ) else (
     echo Python is not installed
     echo Install Python 3.10 by running the installer, then click Yes on the UAC Prompt
-    cd _redist
-    start python-3.10.0-amd64.exe
+    powershell Invoke-WebRequest https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe -O python310.exe
+    start python310.exe 
     PAUSE
-    echo You might need to install from the microsoft store. So install both and go the the microsoft store and search for python 3.1 and you should be fine
+    echo You seem to need to install python twice. This time thru the microsoft store. Press any button to start the MS store and click install
+    start ms-windows-store://pdp/?ProductId=9ncvdn91xzqp
+    PAUSE
 )
 
 echo Both Node.js and Python 3.10 are now (hopefully) installed.
@@ -76,7 +81,7 @@ echo.
 echo "Open OBS, go to the top toolbar and click tools, scripts, Python Settings, and import this following file path:"
 echo "C:/Users/REPLACE THIS WITH YOUR WINDOWS USERNAME/AppData/Local/Programs/Python/Python310 (you will need to click the 'browse' button and insert the path thru the top large search bar)"
 PAUSE 
-echo "Next click apply and go to Scripts, Websocket Server Settings, Enable Websocket Server, set port to 4455, and Disable Authentication, Click apply and ur done with that part."
+echo "Next click apply and go to Tools, Websocket Server Settings, Enable Websocket Server, set port to 4455, and Disable Authentication, Click apply and ur done with that part."
 PAUSE
 echo "Next Open the Scenes folder by clicking any button. When this opens you will need to open the scenes folder in the radar folder. Then copy those json files to the obs scenes folder"
 PAUSE
